2b490914daf9b1b0a8997646494a0b985be57f80
Carlos Almeida <189901081+0xyngtg@users.noreply.github.com>
2026-06-05 16:08:40 +0100

    update tool_runner.py

--- src/tool_runner.py
+++ src/tool_runner.py
@@ -1,86 +1,54 @@
-import re
 import json
+import tool_checker
+import subprocess
+from pathlib import Path
+from dataclasses import dataclass
 
-from abc import ABC, abstractmethod
+CONFIG_FILE: str = "config/tools_config.json"
 
-HEADERS: dict[str, str] = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-}
+TARGET: str = "zara.com"
 
-CONFIG_FILE: str = "config/tools_args.json"
+type Arguments = dict[str, str]
 
-def load_config(config_file: str) -> dict[str, list[str]]:
+@dataclass
+class Tool:
+    name: str
+    path: Path
+    arguments: Arguments
+    
+    def __post_init__(self):
+        for arg, value in self.arguments.items():
+            if value == "target":
+                self.arguments[arg] = TARGET
+            elif value == "output":
+                self.arguments[arg] = f"{self.name}.out"
+            elif value == "token":
+                self.arguments[arg] = ""
+    
+    def run(self) -> subprocess.CompletedProcess:
+        args_list = [self.path] + [f"{k} {v}" for k,v in self.arguments.items()]
+        try:
+            result = subprocess.CompletedProcess = subprocess.run(args_list)
+        except Exception as e:
+            f"Error running {self.name}"
+        return result
+
+def load_config(config_file: str) -> dict[str, dict[str, str]]:
     with open(config_file, "r") as conf:
         return json.load(conf)
 
-class Tool(ABC):
-    def __init__(self, path: str, args: list[str]):
-        self.path = path
-        self.args = args or []
+def main() -> None:
+    tools_local_paths: dict[str, str] = tool_checker.main()
     
-    @abstractmethod
-    def run(self) -> None:
-        pass
-
-class Findomain(Tool):
-    def run(self) -> None:
-        print(f"Running Findomain with {self.path} and {self.args}")
-
-class Waymore(Tool):
-    def run(self) -> None:
-        print(f"Running Waymore with {self.path} and {self.args}")
-
-class Sublister(Tool):
-    def run(self) -> None:
-        print(f"Running Sublist3r with {self.path} and {self.args}")
-
-class GithubSubdomains(Tool):
-    def run(self) -> None:
-        print(f"Running GithubSubdomains with {self.path} and {self.args}")
-
-class Shosubgo(Tool):
-    def run(self) -> None:
-        print(f"Running Shosubgo with {self.path} and {self.args}")
-
-class Assetfinder(Tool):
-    def run(self) -> None:
-        print(f"Running Assetfinder with {self.path} and {self.args}")
-
-def main(tools_local_path: dict[str, str]) -> None:
-    arguments: dict[str, list[str]] = load_config(CONFIG_FILE)
+    tools_arguments: dict[str, Arguments] = load_config(CONFIG_FILE)
     
-    findomain: Findomain = Findomain(
-        path=tools_local_path["findomain"],
-        args=arguments["findomain"]
-    )
-    findomain.run()
-    
-    waymore: Waymore = Waymore(
-        path=tools_local_path["findomain"],
-        args=arguments["waymore"]
-    )
-    waymore.run()
-    
-    sublister: Sublister = Sublister(
-        path=tools_local_path["sublist3r"],
-        args=arguments["sublist3r"]
-    )
-    sublister.run()
-    
-    githubsubdomains: GithubSubdomains = GithubSubdomains(
-        path=tools_local_path["github-subdomains"],
-        args=arguments["github-subdomains"]
-    )
-    githubsubdomains.run()
-    
-    shosubgo: Shosubgo = Shosubgo(
-        path=tools_local_path["shosubgo"],
-        args=arguments["shosubgo"]
-    )
-    shosubgo.run()
-    
-    assetfinder: Assetfinder = Assetfinder(
-        path=tools_local_path["assetfinder"],
-        args=arguments["assetfinder"]
-    )
-    assetfinder.run()
+    for tool, arguments in tools_arguments.items():
+        tool = Tool(
+            name=tool,
+            path=Path(tools_local_paths[tool]),
+            arguments=arguments
+        )
+        tool.run()
+
+if __name__ == "__main__":
+    main()
