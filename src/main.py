07d21965697d7792eb91eed66ac9646015a95dca
Carlos Almeida <189901081+0xyngtg@users.noreply.github.com>
2026-06-08 11:52:00 +0100

    update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import tool_checker
 import tool_runner
 
 def main() -> None:
-    tools_local_paths: dict[str, str] = tool_checker.main()
+    tools_local_paths: dict[str, str] = tool_checker.run()
+    tool_runner.run(tools_local_paths)
 
 if __name__ == "__main__":
     main()
