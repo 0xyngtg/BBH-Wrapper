6311af5e8c691b1ec17d36db34e72247e555a175
Carlos Almeida <189901081+0xyngtg@users.noreply.github.com>
2026-06-10 11:26:53 +0100

    added output processing to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import tool_checker
 import tool_runner
+import output_processing
+
+TARGET: str = input("Target Domain > ")
 
 def main() -> None:
     tools_local_paths: dict[str, str] = tool_checker.run()
-    tool_runner.run(tools_local_paths)
+    tool_runner.run(tools_local_paths=tools_local_paths, target=TARGET)
+    output_processing.run(domain=TARGET)
 
 if __name__ == "__main__":
     main()
