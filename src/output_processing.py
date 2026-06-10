c9ef3b2d96c377d51be5e63ae9807c74c7f9e536
Carlos Almeida <189901081+0xyngtg@users.noreply.github.com>
2026-06-10 11:27:01 +0100

    create output_processing.py

--- src/output_processing.py
+++ src/output_processing.py
@@ -1,1 +1,33 @@
-RESULTS_DIR: str = "results"
+from pathlib import Path
+import re
+
+RESULTS_DIR: Path = Path("results")
+RESULTS_FILE: Path = RESULTS_DIR / Path("results.txt")
+
+def get_result_files(results_dir: Path) -> list[Path]:
+    return list(results_dir.glob("*"))
+
+def get_filtered_content(file_path: Path, pattern: str) -> None:
+    with open(RESULTS_FILE, "a") as r:
+        with open(file_path, "r") as f:
+            content: str = f.read()
+            for line in content.splitlines():
+                if re.match(pattern, line.strip()):
+                    r.write(line + "\n")
+
+def line_counter(file: Path) -> int:
+    line_number: int = 0
+    with open(file, "r") as f:
+        for _ in f:
+            line_number += 1
+    return line_number
+
+def run(domain):
+    pattern: str = fr"^([a-zA-Z0-9-]+\.)+{re.escape(domain)}$"
+    
+    files_list: list[Path] = get_result_files(RESULTS_DIR)
+    for file in files_list:
+        get_filtered_content(file_path=file, pattern=pattern)
+    
+    line_number: int = line_counter(RESULTS_FILE)
+    print(f"[+] {line_number} subdomains were found!")
