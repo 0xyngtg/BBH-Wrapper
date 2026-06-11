60b87bbb5d3cdb1053756955f5023686842ab985
Carlos Almeida <189901081+0xyngtg@users.noreply.github.com>
2026-06-11 11:33:22 +0100

    add run probe_targets function call

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import tool_checker
 import tool_runner
 import output_processing
+import probe_targets
 
 TARGET: str = input("Target Domain > ")
 
@@ -8,6 +9,7 @@ def main() -> None:
     tools_local_paths: dict[str, str] = tool_checker.run()
     tool_runner.run(tools_local_paths=tools_local_paths, target=TARGET)
     output_processing.run(domain=TARGET)
+    probe_targets.run()
 
 if __name__ == "__main__":
     main()
