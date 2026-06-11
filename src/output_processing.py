e15cf3cf0a8a9d5908578885dd95d6bde76fbcad
Carlos Almeida <189901081+0xyngtg@users.noreply.github.com>
2026-06-11 12:57:02 +0100

    fix bug

--- src/output_processing.py
+++ src/output_processing.py
@@ -3,17 +3,17 @@ import re
 
 RESULTS_DIR: Path = Path("results")
 RESULTS_FILE: Path = RESULTS_DIR / Path("results.txt")
+results: list[str] = []
 
 def get_result_files(results_dir: Path) -> list[Path]:
     return list(results_dir.glob("*"))
 
 def get_filtered_content(file_path: Path, pattern: str) -> None:
-    with open(RESULTS_FILE, "a") as r:
-        with open(file_path, "r") as f:
-            content: str = f.read()
-            for line in content.splitlines():
-                if re.match(pattern, line.strip()):
-                    r.write(line + "\n")
+    with open(file_path, "r") as f:
+        content: str = f.read()
+        for line in content.splitlines():
+            if re.match(pattern, line.strip()):
+                results.append(line+"\n")
 
 def line_counter(file: Path) -> int:
     line_number: int = 0
@@ -22,12 +22,19 @@ def line_counter(file: Path) -> int:
             line_number += 1
     return line_number
 
+def log_results() -> None:
+    with open(RESULTS_FILE, "w") as f:
+        unique_results: list[str] = list(set(results))
+        f.writelines(unique_results)
+
 def run(domain) -> None:
     pattern: str = fr"^([a-zA-Z0-9-]+\.)+{re.escape(domain)}$"
     
     files_list: list[Path] = get_result_files(RESULTS_DIR)
     for file in files_list:
         get_filtered_content(file_path=file, pattern=pattern)
+    
+    log_results()
     
     line_number: int = line_counter(RESULTS_FILE)
     print(f"[+] {line_number} subdomains were found!")
